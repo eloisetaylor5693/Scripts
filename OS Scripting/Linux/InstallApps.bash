9d9a7274819927fbad7fd7e7428efaa580f23fc5
eloisetaylor5693 <--local>
2018-09-13 18:37:18 +0100

    Added silent install option (assume yes) and removed some options that aren't required

--- OS Scripting/Linux/InstallApps.bash
+++ OS Scripting/Linux/InstallApps.bash
@@ -1,22 +1,23 @@
+#!/bin/bash
+
 sudo apt install ubuntu-drivers-common
 sudo apt-get update        # Fetches the list of available updates
 sudo apt-get upgrade       # Strictly upgrades the current packages
 sudo apt-get dist-upgrade  # Installs updates (new ones)
 
 
-sudo apt-get install firefox
-sudo apt-get install rkhunter
-sudo apt-get install chkrootkit
-sudo apt-get install git
+sudo apt-get install -y firefox
+sudo apt-get install -y rkhunter
+sudo apt-get install -y chkrootkit
+sudo apt-get install -y git
 
 # Node
 ## https://nodejs.org/en/download/package-manager/
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install -y npm
 
-# fix issue where linux can't find node, because it's named nodejs
-sudo ln -s /usr/bin/nodejs /usr/bin/node
+# fix issue where linux can't find node, because it's sometimes named nodejs
+#sudo ln -s /usr/bin/nodejs /usr/bin/node
 
 
 # VS Code 
@@ -25,10 +26,10 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-sudo apt-get install apt-transport-https
+sudo apt-get install -y apt-transport-https
 sudo apt-get update
-sudo apt-get install code
+sudo apt-get install -y code
 
 
-# Cleanup packages that are not required
-sudo apt autoremove        
+# Cleanup packages that are not required (reboot first)
+#sudo apt autoremove 
