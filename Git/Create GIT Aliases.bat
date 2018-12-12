4dfd0293f75adeffd5346e34e9517323dc7a02b7
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2018-12-12 09:40:53 +0000

    Added alias to clean unstaged changes

--- Git/Create GIT Aliases.bat
+++ Git/Create GIT Aliases.bat
@@ -23,6 +23,8 @@ git config --global alias.br branch
 
 git config --global alias.f "!git fetch --all --tags --progress && git status "
 
+git config --global alias.cleanunstaged "!git reset --hard && git clean -dfx "
+
 git config --global alias.fdr "fetch --dry-run --progress" 
 
 git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short" 
