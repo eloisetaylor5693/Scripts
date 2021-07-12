bdd4d666108251a47dc283b6f3528925fab6ac92
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-07-12 14:28:15 +0100

    included bin and obj folders

--- OS Scripting/Powershell/Delete all folders with name.ps1
+++ OS Scripting/Powershell/Delete all folders with name.ps1
@@ -5,12 +5,12 @@ Set-Location -Path "C:\Code"
 if ($DoDryRun) 
 {
     Write-Host Dry run
-    Get-ChildItem . -Include packages,node_modules -Recurse -Force | Remove-Item -Recurse -Force -WhatIf
+    Get-ChildItem . -Include packages,node_modules,bin,obj -Recurse -Force | Remove-Item -Recurse -Force -WhatIf
 }
 else 
 {
     Write-Host Deleting
-    Get-ChildItem . -Include packages,node_modules -Recurse -Force | Remove-Item -Recurse -Force
+    Get-ChildItem . -Include packages,node_modules,bin,obj -Recurse -Force | Remove-Item -Recurse -Force
 }
 
 Write-Host Done
