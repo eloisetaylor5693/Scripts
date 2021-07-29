100726b57d868c0a695885b246fdce2acc95fbce
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-07-29 14:53:41 +0100

    converted git aliases to unix script

---
+++ Git/create-git-aliases.bash
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+echo Removing current global aliases 
+
+git config --global --remove-section alias 
+
+echo Adding aliase 
+
+git config --global alias.ad "add -A" 
+
+git config --global alias.pl "pull --no-rebase" 
+
+git config --global alias.plr "pull --rebase" 
+
+git config --global alias.plf "pull --ff-only" 
+
+git config --global alias.co checkout 
+git config --global alias.cob "co -b"
+git config --global alias.com "co main"
+
+git config --global alias.mm "merge main"
+
+
+git config --global alias.f '!git fetch --all --tags --progress --prune; git status'
+git config --global alias.st f 
+
+git config --global alias.undounstaged "!git reset --hard && git clean -dfx "
+
+git config --global alias.fdr "fetch --dry-run --progress" 
+
+
+git config --global alias.type "cat-file -t" 
+
+git config --global alias.dump "cat-file -p"
+
+
+echo Configured global aliases 
+
+git config --global --get-regexp alias 
