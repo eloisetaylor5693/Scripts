5b2169e1de4396eac9786cdd8b7e1f2e6926385f
eloisetaylor5693 <eloisetaylor5693@gmail.com>
2021-07-29 14:54:13 +0100

    added extra aliases found on the internet: https://www.makeuseof.com/how-to-use-git-aliases-10-essential-examples/

--- Git/create-git-aliases.bash
+++ Git/create-git-aliases.bash
@@ -24,7 +24,11 @@ git config --global alias.mm "merge main"
 git config --global alias.f '!git fetch --all --tags --progress --prune; git status'
 git config --global alias.st f 
 
+git config --global alias.b '!git for-each-ref --sort="-authordate" --format="%(authordate)%09%(objectname:short)%09%(refname)" refs/heads | sed -e "s-refs/heads/--"'
+git config --global alias.br "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
+
 git config --global alias.undounstaged "!git reset --hard && git clean -dfx "
+git config --global alias.undo 'reset --soft HEAD~1'
 
 git config --global alias.fdr "fetch --dry-run --progress" 
 
@@ -32,6 +36,9 @@ git config --global alias.fdr "fetch --dry-run --progress"
 git config --global alias.type "cat-file -t" 
 
 git config --global alias.dump "cat-file -p"
+
+git config --global alias.contrib 'shortlog -e --summary --numbered'
+git config --global alias.ll 'log --oneline'
 
 
 echo Configured global aliases 
